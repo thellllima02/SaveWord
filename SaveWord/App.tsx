495e24aeaed2c67139cbfdb78ce8253a4f6e3799
Hércules <116934166+thellllima02@users.noreply.github.com>
2024-10-30 14:06:42 -0300

    .

--- SaveWord/App.tsx
+++ SaveWord/App.tsx
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native'
 
-export default function App() {
-  return (
+export default function App(){
+  return(
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      <Text>Save PassWord</Text>
+      <TouchableOpacity style={styles.button}>
+        <Text style = {styles.buttonText}>Aceitar</Text>
+      </TouchableOpacity>
+
+    </View>  
+  )  
 }
+    
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    flex:1,
+    backgroundColor: "#FFFFFF",
+    justifyContent: 'flex-start',
+    alignItems: 'center'
   },
-});
+  button:{
+    backgroundColor: "red",
+  },
+  buttonText:{
+    color:"white",
+  }
+})
+
